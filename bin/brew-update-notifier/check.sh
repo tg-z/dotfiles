d7f3d58b51d663e82d5daf6cb6813ad9a66c30b6
tg-z <parasitism@protonmail.com>
2021-01-30 04:36:49 -0800

    updated brew notification scripts

--- bin/brew-update-notifier/check.sh
+++ bin/brew-update-notifier/check.sh
@@ -8,26 +8,24 @@
 
 PATH=/usr/local/bin:$PATH
 
-BREW_EXEC='/usr/local/bin/brew'
-TERMINAL_NOTIFIER=`which terminal-notifier`
-SCRIPT_DIR=`dirname $0`
-DO_UPGRADE="$SCRIPT_DIR/do-upgrade.sh"
-LOG="$SCRIPT_DIR/log.txt"
-DATE=`date +%Y-%m-%dT%H:%M:%S`
-echo $DATE " Checking for updates" >> $LOG
+BREW_EXEC="$(command -v brew)"
+TERMINAL_NOTIFIER="$(command -v terminal-notifier)"
+SCRIPT_DIR="$(dirname "${0}")"
+DO_UPGRADE="${SCRIPT_DIR}/do-upgrade.sh"
+LOG="${SCRIPT_DIR}/log.txt"
+DATE="$(date "+%Y-%m-%d %I:%M %p")"
+echo "${DATE}: Checking for updates..." >> "${LOG}"
 
-$BREW_EXEC update 2>&1 > /dev/null
-outdated=`$BREW_EXEC outdated | tr ' ' '\n'`
+"${BREW_EXEC}" update > /dev/null 2>&1
+outdated="$("${BREW_EXEC}" outdated | tr ' ' '\n')"
 
-echo "Found outdated ----" >> $LOG
-echo $outdated >> $LOG
-echo "----" >> $LOG
 
-if [ -z "$outdated" ] ; then
-    if [ -e $TERMINAL_NOTIFIER ]; then
+if [ -z "${outdated}" ] ; then
+    if [ -e "${TERMINAL_NOTIFIER}" ]; then
 	echo "no updates"
-        #No updates available
-        # $TERMINAL_NOTIFIER $NOTIF_ARGS \
+	echo "---" >> "${LOG}"
+        # No updates available
+        # "${TERMINAL_NOTIFIER}" "${NOTIF_ARGS}" \
         #   -title "No Homebrew Updates Available" \
         #    -message "No updates available yet for any homebrew packages."
     fi
@@ -35,10 +33,15 @@ else
     # We've got an outdated formula or two
 
     # Nofity via Notification Center
-    if [ -e $TERMINAL_NOTIFIER ]; then
-        lc=$((`echo "$outdated" | wc -l`))
-        outdated=`echo "$outdated" | tail -$lc`
-        message=`echo "$outdated" | head -5`
+    if [ -e "${TERMINAL_NOTIFIER}" ]; then
+	    {
+	      echo "Found outdated:" >> "${LOG}"
+	      echo "${outdated}" >> "${LOG}"
+	      echo "---"
+            } >> "${LOG}"
+	    lc="$(("$(echo "$outdated" | wc -l)"))"
+	outdated="$(echo "${outdated}" | tail -$lc)"
+	message="$(echo "${outdated}" | head -5)"
         if [ "$outdated" != "$message" ]; then
             message="Some of the outdated formulae are:
 $message"
@@ -47,6 +50,6 @@ $message"
 $message"
         fi
         # Send to the Nofication Center
-        $TERMINAL_NOTIFIER -execute $DO_UPGRADE -title "Homebrew Updates Available" -message "$message" -timeout 10
+        "${TERMINAL_NOTIFIER}" -execute "${DO_UPGRADE}" -title "Homebrew Updates Available" -message "${message}" -timeout 10
     fi
 fi
--- bin/brew-update-notifier/do-upgrade.sh
+++ bin/brew-update-notifier/do-upgrade.sh
@@ -2,16 +2,16 @@
 
 PATH=/usr/local/bin:$PATH
 
-TERMINAL_NOTIFIER=`which terminal-notifier`
-BREW_EXEC='/usr/local/bin/brew'
-SCRIPT_DIR=`dirname $0`
-LOG="$SCRIPT_DIR/log.txt"
-DATE=`date +%Y-%m-%dT%H:%M:%S`
+TERMINAL_NOTIFIER="$(command -v terminal-notifier)"
+BREW_EXEC="$(command -v brew)"
+SCRIPT_DIR="$(dirname "${0}")"
+LOG="${SCRIPT_DIR}/log.txt"
+DATE="$(date +%Y-%m-%dT%H:%M:%S)"
 
-echo "$DATE Performing update" >> $LOG
+echo "${DATE} Performing update" >> "${LOG}"
 
-output=`$BREW_EXEC upgrade`
+output="$(${BREW_EXEC} upgrade)"
 
-echo $output >> $LOG
+echo "${output}" >> "${LOG}"
 
-$TERMINAL_NOTIFIER -title "Homebrew Upgrade Done" -message "$output"
+"${TERMINAL_NOTIFIER}" -title "Homebrew Upgrade Done" -message "${output}"
